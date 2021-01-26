57b999b4b45d3bceef5dd728302237bbc4706256
Alexander Baquiax <alexanderbaquiax@gmail.com>
2021-01-25 22:03:21 -0600

    feat: implement SortableJS to output words

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import OutputWords from './components/OutputWords';
 
-const words = [
+const defaultWords = [
   {
+    id: '1',
     tag: 'NOUN',
     type: 'C',
     properties: {
@@ -11,6 +12,47 @@ const words = [
     },
   },
   {
+    id: '2',
+    tag: 'NOUN',
+    type: 'P',
+    properties: {
+      type: 'P',
+    },
+  },
+  {
+    id: '3',
+    tag: 'NOUN',
+    type: 'P',
+    properties: {
+      type: 'P',
+    },
+  },
+  {
+    id: '4',
+    tag: 'NOUN',
+    type: 'P',
+    properties: {
+      type: 'P',
+    },
+  },
+  {
+    id: '5',
+    tag: 'NOUN',
+    type: 'P',
+    properties: {
+      type: 'P',
+    },
+  },
+  {
+    id: '6',
+    tag: 'NOUN',
+    type: 'P',
+    properties: {
+      type: 'P',
+    },
+  },
+  {
+    id: '7',
     tag: 'NOUN',
     type: 'P',
     properties: {
@@ -19,8 +61,9 @@ const words = [
   },
 ];
 
-const App = () => (
-  <OutputWords words={words} />
-);
+const App = () => {
+  const [words, setWords] = React.useState(defaultWords);
+  return <OutputWords words={words} onChange={(newWords) => { setWords(newWords); }} />;
+};
 
 export default App;
